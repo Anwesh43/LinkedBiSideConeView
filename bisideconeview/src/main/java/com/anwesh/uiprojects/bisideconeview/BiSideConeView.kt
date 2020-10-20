304acfffc185feef5f0e46c066c55a69c41007bd
Anwesh43 <anweshthecool0@gmail.com>
2020-10-21 04:10:02 +0530

    adding constants and libraries

--- bisideconeview/src/main/java/com/anwesh/uiprojects/bisideconeview/BiSideConeView.kt
+++ bisideconeview/src/main/java/com/anwesh/uiprojects/bisideconeview/BiSideConeView.kt
@@ -3,3 +3,30 @@ package com.anwesh.uiprojects.bisideconeview
 /**
  * Created by anweshmishra on 21/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.graphics.RectF
+
+val parts : Int = 6
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.4f
+val delay : Long = 20
+val deg : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#2196F3",
+        "#FF9800",
+        "#3F51B5"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
