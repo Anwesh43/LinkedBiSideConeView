d0736b90a0c91ddf018750ad875f02d790715c0c
Anwesh43 <anweshthecool0@gmail.com>
2020-10-21 04:31:26 +0530

    adding Renderer class

--- bisideconeview/src/main/java/com/anwesh/uiprojects/bisideconeview/BiSideConeView.kt
+++ bisideconeview/src/main/java/com/anwesh/uiprojects/bisideconeview/BiSideConeView.kt
@@ -209,4 +209,27 @@ class BiSideConeView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiSideConeView) {
+
+        private val animator : Animator = Animator(view)
+        private val bsc : BiSideCone = BiSideCone(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bsc.draw(canvas, paint)
+            animator.animate {
+                bsc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bsc.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
