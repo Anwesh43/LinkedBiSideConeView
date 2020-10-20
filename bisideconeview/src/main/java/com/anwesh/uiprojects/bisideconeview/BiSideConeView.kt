54b50dcb2af4bae5aa8b471e22ac0e50b4b4a69f
Anwesh43 <anweshthecool0@gmail.com>
2020-10-21 04:21:09 +0530

    adding Canvas extension methods

--- bisideconeview/src/main/java/com/anwesh/uiprojects/bisideconeview/BiSideConeView.kt
+++ bisideconeview/src/main/java/com/anwesh/uiprojects/bisideconeview/BiSideConeView.kt
@@ -35,3 +35,50 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawConeFillPath(scale : Float, r : Float, paint : Paint) {
+    paint.style = Paint.Style.FILL
+    save()
+    val path : Path = Path()
+    path.moveTo(0f, 0f)
+    path.lineTo(-r, r)
+    path.arcTo(RectF(-2 * r, -r, 0f, r), deg * 2, deg * 2, false)
+    path.lineTo(0f, 0f)
+    clipPath(path)
+    drawRect(RectF(-2 * r * scale, -r, 0f, r), paint)
+    restore()
+}
+
+fun Canvas.drawBiSideCone(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val sf5 : Float = sf.divideScale(4, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = size / 2
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sf5)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        paint.style = Paint.Style.STROKE
+        for (i in 0..1) {
+            val sfi : Float = sf.divideScale(i * 2, parts)
+            drawLine(-r * sfi, r * (1f - 2 * i) * sfi, 0f, 0f, paint)
+        }
+        drawArc(RectF(-2 * r, -r, 0f, r), deg * 2, deg * 2 * sf2, false, paint)
+        drawConeFillPath(sf4, r, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiSideCone(scale, w, h, paint)
+}
